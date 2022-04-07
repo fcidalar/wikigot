c2c273cb74014f9f9f2d16dfa11e0a732d4ba2bf
Fabian Cid <fabian.cid@parquedelrecuerdo.cl>
2022-04-07 11:56:11 -0400

    (feat) Se agrega alert para rutas no autorizadas y nuevas imagenes

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,19 +1,43 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { Observable } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
-  constructor(private auth: AuthService, private router: Router ){}
+  constructor(private auth: AuthService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> {
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+
+    this.auth.isAuthenticated$.subscribe({
+      next: (isAuth) => {
+
+        if (!isAuth) {
+          this.router.navigateByUrl('/home');
+
+          Swal.fire({
+            title: 'Alto ahí!',
+            text: 'Debes estar logueado para acceder a esta ruta.',
+            imageUrl: './assets/images/slap-game-of-thrones.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+          });
+        }
+      },
+    });
+
     return this.auth.isAuthenticated$;
   }
-
 }
